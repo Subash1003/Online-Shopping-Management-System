09e8aa01ed1fe045247405fd44741e1517fb7932
Subash <161806495+Subash1003@users.noreply.github.com>
2025-06-06 01:06:45 +0530

    deleted comment lines in DBConnection.java

--- src/onlineshoppingmanagement/DBConnection.java
+++ src/onlineshoppingmanagement/DBConnection.java
@@ -1,19 +1,3 @@
-//package onlineshoppingmanagement;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//
-//public class DBConnection {
-//    private static final String DB_URL = "jdbc:mysql://localhost:3306/onlineshoppingmanagement";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "Subash@2003"; // Replace with your MySQL password
-//
-//    public static Connection getConnection() throws SQLException {
-//        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
-//    }
-//}
-
 
 package onlineshoppingmanagement;
 import java.sql.*;
